afcc5989e09909df9c9a9902f1262c9b98e08737
AlexanderDerevliov <alex.feelred@gmail.com>
2024-01-11 18:28:01 +0100

    exercise complited

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $page_greetings = 'Hello Laravel';
+
+    $data = [
+        'title' => $page_greetings,
+    ];
+
+    return view('home ' , $data);
 });
